c00ca481cde62e0d1f6c5d35a4735e4f0c757832
thebaolin <bchang916@gmail.com>
2022-11-10 15:47:53 -0500

    updated

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
 
 // add functions here
+
+double length(Coord3D *p){
+    return sqrt(pow(p->x,2) + pow(p->y,2) + pow(p->z,2));
+}
+
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    if(length(p1) > length(p2))
+        return p1;
+    else
+        return p2;
+}
+
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+    ppos->x += ppos->x + pvel->x + dt;
+    ppos->y += ppos->y + pvel->y + dt;
+    ppos->z += ppos->z + pvel->z + dt;
+}
+
+
 
